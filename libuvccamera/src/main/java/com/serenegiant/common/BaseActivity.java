a50c06e3b5084b4b99016acb1a690d2980a10225
Solomatov A.A <solomatov.andrew@nami.ru>
2021-09-24 14:05:10 +0400

    - Updated code base to commit 2e8f690b49a7d40c7 of 17 nov 2016,    'add BaseActivity to help to support dynamic permission model'.     Still works a little incorrectly. - Translate comments to english in file:     libuvccamera/(...)serenegiant/common/BaseActivity.java

--- libuvccamera/src/main/java/com/serenegiant/common/BaseActivity.java
+++ libuvccamera/src/main/java/com/serenegiant/common/BaseActivity.java
@@ -1,10 +1,319 @@
 package com.serenegiant.common;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.serenegiant.dialog.MessageDialogFragment;
+import com.serenegiant.utils.BuildCheck;
+import com.serenegiant.utils.HandlerThreadHandler;
+import com.serenegiant.utils.PermissionCheck;
 
 /**
  * Created by saki on 2016/11/18.
+ *
  */
+public class BaseActivity extends Activity
+	implements MessageDialogFragment.MessageDialogListener {
 
-public class BaseActivity extends Activity {
+	private static boolean DEBUG = false;	// FIXME 実働時はfalseにセットすること
+	private static final String TAG = BaseActivity.class.getSimpleName();
+
+	/** UI操作のためのHandler */
+	private final Handler mUIHandler = new Handler(Looper.getMainLooper());
+	private final Thread mUiThread = mUIHandler.getLooper().getThread();
+	/** ワーカースレッド上で処理するためのHandler */
+	private Handler mWorkerHandler;
+	private long mWorkerThreadID = -1;
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// ワーカースレッドを生成
+		if (mWorkerHandler == null) {
+			mWorkerHandler = HandlerThreadHandler.createHandler(TAG);
+			mWorkerThreadID = mWorkerHandler.getLooper().getThread().getId();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		clearToast();
+		super.onPause();
+	}
+
+	@Override
+	protected synchronized void onDestroy() {
+		// ワーカースレッドを破棄
+		if (mWorkerHandler != null) {
+			try {
+				mWorkerHandler.getLooper().quit();
+			} catch (final Exception e) {
+				//
+			}
+			mWorkerHandler = null;
+		}
+		super.onDestroy();
+	}
+
+//================================================================================
+	/**
+	 * Helper method for running Runnable in UI thread
+	 * @param task
+	 * @param duration
+	 */
+	public final void runOnUiThread(final Runnable task, final long duration) {
+		if (task == null) return;
+		mUIHandler.removeCallbacks(task);
+		if ((duration > 0) || Thread.currentThread() != mUiThread) {
+			mUIHandler.postDelayed(task, duration);
+		} else {
+			try {
+				task.run();
+			} catch (final Exception e) {
+				Log.w(TAG, e);
+			}
+		}
+	}
+
+	/**
+	 * If the Runnable specified on the UI thread is waiting for execution,
+	 * cancel the execution wait.
+	 * @param task
+	 */
+	public final void removeFromUiThread(final Runnable task) {
+		if (task == null) return;
+		mUIHandler.removeCallbacks(task);
+	}
+
+	/**
+	 * Execute the specified Runnable on the worker thread
+	 * If there is the same Runnable that has not been executed,
+	 * it will be canceled (only the one specified later will be executed)
+	 * @param task
+	 * @param delayMillis
+	 */
+	protected final synchronized void queueEvent(final Runnable task, final long delayMillis) {
+		if ((task == null) || (mWorkerHandler == null)) return;
+		try {
+			mWorkerHandler.removeCallbacks(task);
+			if (delayMillis > 0) {
+				mWorkerHandler.postDelayed(task, delayMillis);
+			} else if (mWorkerThreadID == Thread.currentThread().getId()) {
+				task.run();
+			} else {
+				mWorkerHandler.post(task);
+			}
+		} catch (final Exception e) {
+			// ignore
+		}
+	}
+
+	/**
+	 * Cancel the specified Runnable if it is scheduled
+	 * to run on the worker thread
+	 * @param task
+	 */
+	protected final synchronized void removeEvent(final Runnable task) {
+		if (task == null) return;
+		try {
+			mWorkerHandler.removeCallbacks(task);
+		} catch (final Exception e) {
+			// ignore
+		}
+	}
+
+//================================================================================
+	private Toast mToast;
+	/**
+	 * Show message in Toast
+	 * @param msg
+	 */
+	protected void showToast(@StringRes final int msg, final Object... args) {
+		removeFromUiThread(mShowToastTask);
+		mShowToastTask = new ShowToastTask(msg, args);
+		runOnUiThread(mShowToastTask, 0);
+	}
+
+	/**
+	 * Cancel showing in Toast
+	 */
+	protected void clearToast() {
+		removeFromUiThread(mShowToastTask);
+		mShowToastTask = null;
+		try {
+			if (mToast != null) {
+				mToast.cancel();
+				mToast = null;
+			}
+		} catch (final Exception e) {
+			// ignore
+		}
+	}
+
+	private ShowToastTask mShowToastTask;
+	private final class ShowToastTask implements Runnable {
+		final int msg;
+		final Object args;
+		private ShowToastTask(@StringRes final int msg, final Object... args) {
+			this.msg = msg;
+			this.args = args;
+		}
+
+		@Override
+		public void run() {
+			try {
+				if (mToast != null) {
+					mToast.cancel();
+					mToast = null;
+				}
+				if (args != null) {
+					final String _msg = getString(msg, args);
+					mToast = Toast.makeText(BaseActivity.this, _msg, Toast.LENGTH_SHORT);
+				} else {
+					mToast = Toast.makeText(BaseActivity.this, msg, Toast.LENGTH_SHORT);
+				}
+				mToast.show();
+			} catch (final Exception e) {
+				// ignore
+			}
+		}
+	}
+
+//================================================================================
+	/**
+	 * MessageDialogFragment Callback Listener from Message Dialog
+	 * @param dialog
+	 * @param requestCode
+	 * @param permissions
+	 * @param result
+	 */
+	@SuppressLint("NewApi")
+	@Override
+	public void onMessageDialogResult(final MessageDialogFragment dialog, final int requestCode, final String[] permissions, final boolean result) {
+		if (result) {
+			// Request permissions when OK is pressed in the message dialog
+			if (BuildCheck.isMarshmallow()) {
+				requestPermissions(permissions, requestCode);
+				return;
+			}
+		}
+		// When canceled in the message dialog and when it is not Android 6,
+		// check it yourself and call #checkPermissionResult
+		for (final String permission: permissions) {
+			checkPermissionResult(requestCode, permission, PermissionCheck.hasPermission(this, permission));
+		}
+	}
+
+	/**
+	 * Method for receiving permission request result
+	 * @param requestCode
+	 * @param permissions
+	 * @param grantResults
+	 */
+	@Override
+	public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);	// 何もしてないけど一応呼んどく
+		final int n = Math.min(permissions.length, grantResults.length);
+		for (int i = 0; i < n; i++) {
+			checkPermissionResult(requestCode, permissions[i], grantResults[i] == PackageManager.PERMISSION_GRANTED);
+		}
+	}
+
+	/**
+	 * Check the result of a permission request
+	 * Here, just display a message in Toast when permission cannot be obtained
+	 * @param requestCode
+	 * @param permission
+	 * @param result
+	 */
+	protected void checkPermissionResult(final int requestCode, final String permission, final boolean result) {
+		// Show message when you don't have permission
+		if (!result && (permission != null)) {
+			if (Manifest.permission.RECORD_AUDIO.equals(permission)) {
+				showToast(R.string.permission_audio);
+			}
+			if (Manifest.permission.WRITE_EXTERNAL_STORAGE.equals(permission)) {
+				showToast(R.string.permission_ext_storage);
+			}
+			if (Manifest.permission.INTERNET.equals(permission)) {
+				showToast(R.string.permission_network);
+			}
+		}
+	}
+
+	// Request code for dynamic permission request
+	protected static final int REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 0x12345;
+	protected static final int REQUEST_PERMISSION_AUDIO_RECORDING = 0x234567;
+	protected static final int REQUEST_PERMISSION_NETWORK = 0x345678;
+	protected static final int REQUEST_PERMISSION_CAMERA = 0x537642;
+
+	/**
+	 * Check if you have write permission to external storage
+	 * If not, the explanation dialog is displayed.
+	 * @return true You have write permission to external storage
+	 */
+	protected boolean checkPermissionWriteExternalStorage() {
+		if (!PermissionCheck.hasWriteExternalStorage(this)) {
+			MessageDialogFragment.showDialog(this, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE,
+				R.string.permission_title, R.string.permission_ext_storage_request,
+				new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE});
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Check if you have recording permission
+	 * If not, the explanation dialog is displayed.
+	 * @return true You have recording permission
+	 */
+	protected boolean checkPermissionAudio() {
+		if (!PermissionCheck.hasAudio(this)) {
+			MessageDialogFragment.showDialog(this, REQUEST_PERMISSION_AUDIO_RECORDING,
+				R.string.permission_title, R.string.permission_audio_recording_request,
+				new String[]{Manifest.permission.RECORD_AUDIO});
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Check if you have network access permissions
+	 * If not, the explanation dialog is displayed.
+	 * @return true You have network access permissions
+	 */
+	protected boolean checkPermissionNetwork() {
+		if (!PermissionCheck.hasNetwork(this)) {
+			MessageDialogFragment.showDialog(this, REQUEST_PERMISSION_NETWORK,
+				R.string.permission_title, R.string.permission_network_request,
+				new String[]{Manifest.permission.INTERNET});
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Check if you have permission to access the camera
+	 * If not, the explanation dialog is displayed.
+	 * @return true You have permission to access the camera
+	 */
+	protected boolean checkPermissionCamera() {
+		if (!PermissionCheck.hasCamera(this)) {
+			MessageDialogFragment.showDialog(this, REQUEST_PERMISSION_CAMERA,
+				R.string.permission_title, R.string.permission_camera_request,
+				new String[]{Manifest.permission.CAMERA});
+			return false;
+		}
+		return true;
+	}
+
 }
